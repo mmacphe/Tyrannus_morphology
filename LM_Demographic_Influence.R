2207e4c92b2c65d5efcc832797dcf3be018c25a6
Maggie MacPherson <maggie.macpherson@gmail.com>
2021-02-27 15:16:42 -0600

    Updated code and moved output files for demographic accountability
    Updated .R code "LM_Demographic_Influence" to be more elegant and produce a .txt file showing the differences in AIC values for linear models accounting for SEX. Moved output files (.txt file & Voucher tables for each sex to be used for independent analyses shown in supplementary material) into Output Files folder.

--- LM_Demographic_Influence.R
+++ LM_Demographic_Influence.R
@@ -1,92 +1,85 @@
 rm(list=ls())
-getwd()
 
->library(readx1)
-Tyrannus_voucher_table<-read_excel("C:/Users/maggi/Downloads/Tyrannus voucher table (1).xlsx")
-Morph_data<-Tyrannus_voucher_table_uk_removed
+### Set source directory to the folder this file came from within RStudio
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-#What we're trying to do is to see if:
-#1) morphologies differ between the sexes (in each taxon?)
-#2) morphologies differ between age classes (in each taxon?)
-#3) morphologies vary with body size (in each taxon)
+morpho<-read.csv("Tyrannus_voucher_table.csv")
 
-#First step is to classify each taxon 
-Morph_data$subspecies<-trimws(Morph_data$subspecies)
-Morph_data$trinomial<-paste(Morph_data$Species,Morph_data$subspecies)
-Morph_data$trinomial<-trimws(Morph_data$trinomial)
-table(Morph_data$trinomial) #unknown subspecies were removed
+#What we're trying to do is to see if, within each taxon:
+#1) morphologies differ between the sexes, and 
+#2) morphologies differ between age classes
 
-#/\/\/\/\/# Q1) Do the morphologies differ between the sexes? #unknown sex were removed
-Morph_data<-Morph_data[!Morph_data$Sex=="",]
-Morph_data$Sex[Morph_data$Sex=="H"]<-"Female"
-Morph_data$Sex[Morph_data$Sex=="M"]<-"Male"
-Morph_data$Sex[Morph_data$Sex=="D"]<-"Female"
-Morph_data$Sex[Morph_data$Sex=="F"]<-"Female"
-Morph_data$Sex[Morph_data$Sex=="Female "]<-"Female"
-Morph_data$Sex[Morph_data$Sex=="NA"]<-"Unknown"
-Morph_data$Sex[Morph_data$Sex=="unknown"]<-"Unknown"
-table(Morph_data$Sex)
+### Remove any individuals with unresolved taxonomic identities
+morpho<-morpho[-grep("unknown",morpho$subspecies),]
 
-lm1<-lm(Morph_data$`BL Average` ~ Morph_data$trinomial, data=Morph_data)
-lm2<-lm(Morph_data$`BL Average`~ Morph_data$trinomial*Morph_data$Sex)
+### Classify each taxon using trinomial naming
+morpho$subspecies<-trimws(morpho$subspecies)
+morpho$trinomial<-paste(morpho$Species,morpho$subspecies)
+morpho$trinomial<-trimws(morpho$trinomial)
+names(table(morpho$trinomial)) #see that unknown subspecies were removed
 
-AIC(lm1, lm2)
-#    df      AIC
-#lm1 29 4072.592
-#lm2 57 4086.302
+### Standardize notation for Sex column
+names(table(morpho$Sex))
+morpho<-morpho[!morpho$Sex=="Unknown",] #removes unknown sex individuals
+names(table(morpho$Sex)) #should now only have "Female" and "Male"
 
-lm1<-lm(Morph_data$`BW Average` ~ Morph_data$trinomial, data=Morph_data)
-lm2<-lm(Morph_data$`BW Average`~ Morph_data$trinomial*Morph_data$Sex)
+### For Use in later steps: Make separate male and female voucher tables
+morpho_Females<-morpho[!morpho$Sex=="Male",]
+write.csv(morpho_Females, file="Voucher_Table_Females.csv")
 
-AIC(lm1, lm2)
-#    df      AIC
-#lm1 29 2409.433
-#lm2 57 2415.325
+morpho_Males<-morpho[!morpho$Sex=="Female",]
+write.csv(morpho_Males, file="Voucher_Table_Males.csv")
 
-lm1<-lm(Morph_data$`BD Average` ~ Morph_data$trinomial, data=Morph_data)
-lm2<-lm(Morph_data$`BD Average`~ Morph_data$trinomial*Morph_data$Sex)
+response_var<-colnames(morpho)[c(18,19,20,21,22,23,24)]
 
-AIC(lm1, lm2)
-#    df      AIC
-#lm1 29 1483.731
-#lm2 57 1478.459
+#Create empty list to store output of each for loop iteration
+LM1_output<-list()
+LM2_output<-list()
 
-lm1<-lm(Morph_data$`Kipp's Average` ~ Morph_data$trinomial, data=Morph_data)
-lm2<-lm(Morph_data$`Kipp's Average`~ Morph_data$trinomial*Morph_data$Sex)
+#Conduct the phylogenetic ANOVA he following code works without assuming y is the same order as tree$tip.label
+for(i in 1:length(response_var)){
+  print(paste0("Character",i," -- ",response_var[i]))#Report character
+  character_of_interest<-morpho[,response_var[i]] #Extract character of interest (coi)
+  names(character_of_interest)<-morpho$trinomial #Add names to vector of coi
+  LM1_output[[i]]<-AIC(lm(morpho[,response_var[i]]~morpho$trinomial, data=morpho)) #Perform phylogenetic residuals
+  LM2_output[[i]]<-AIC(lm(morpho[,response_var[i]]~morpho$trinomial*morpho$Sex, data=morpho))
+}
 
-AIC(lm1, lm2)
-#    df      AIC
-#lm1 29 5903.397
-#lm2 57 5528.015
+names(LM1_output)<-response_var
+names(LM2_output)<-response_var 
 
-lm1<-lm(Morph_data$`WC Average` ~ Morph_data$trinomial, data=Morph_data)
-lm2<-lm(Morph_data$`WC Average`~ Morph_data$trinomial*Morph_data$Sex)
+for(i in 1:length(response_var)){
+  print(paste0("Character",i," -- ",response_var[[i]]))
+  AIC_Diff[[i]]<-LM1_output[[i]]-LM2_output[[i]]
+}
 
-AIC(lm1, lm2)
-#    df      AIC
-#lm1 29 7943.603
-#lm2 57 7813.485
+names(AIC_Diff)<-response_var
 
-lm1<-lm(Morph_data$Tail ~ Morph_data$trinomial, data=Morph_data)
-lm2<-lm(Morph_data$Tail ~ Morph_data$trinomial*Morph_data$Sex)
+### Write out output ###
+sink("Sex_Influence.txt")
+for(i in 1:length(AIC_Diff)){
+  cat(paste0("Character ",i," -- ",response_var[i]))
+  cat("\n\n")
+  print(AIC_Diff[[i]])
+  cat("#############")
+  cat("\n\n")
+}
+sink()
+#Note: For all values >2 AIC units, one model is better than the other.
+#Here, we subtracted LM2 (accounted for sex) from LM1 (did not account for sex).
+#All negative values indicate that the model was improved by the accounting of sex, and positive values that it was not.
 
-AIC(lm1, lm2)
-#    df       AIC
-#lm1 29 10195.159
-#lm2 57  9657.571
 
 #/\/\/\/\/#Q2) Do the morphologies differ between age classes?
-Morph_data<-Morph_data[!Morph_data$Age=="",]
-Morph_data$Age[Morph_data$Age=="Adult "]<-"Adult"
-Morph_data$Age[Morph_data$Age=="AD"]<-"Adult"
-Morph_data$Age[Morph_data$Age=="SY"]<-"Adult"
-Morph_data$Age[Morph_data$Age=="nestling"]<-"Nestling"
-Morph_data$Age[Morph_data$Age=="JV"]<-"Juvenile"
-Morph_data$Age[Morph_data$Age=="HY"]<-"Juvenile"
-Morph_data<-Morph_data[!Morph_data$Age=="Nestling",]
-Morph_data<-Morph_data[!Morph_data$Age=="Fledgling",]
-names(table(Morph_data$Age)) #unknown + Juvenile (n=267) removed
-table(Morph_data$Age)
+### Standardize notation for Age column
+names(table(morpho$Age)) #to see which assignments are in the Age column
+morpho$Age[morpho$Age=="SecondYear"]<-"Adult"
+morpho$Age[morpho$Age=="Fledgling"]<-"Juvenile"
+morpho$Age[morpho$Age=="HatchYear"]<-"Juvenile"
+morpho$Age[morpho$Age=="Nestling"]<-"Juvenile"
+morpho<-morpho[!morpho$Age=="Unknown",] #removes unknown age individuals
+names(table(morpho$Age)) #should now only have "Adult" and "Juvenile"
+
 #There is only one age category (Adult) so LM1 should always be better than LM2
 
 lm1<-lm(Morph_data$`BL Average` ~ Morph_data$trinomial, data=Morph_data)
