85c00df58a2f98f4982a3766b5d820fac54d14ea
Maggie MacPherson <maggie.macpherson@gmail.com>
2021-07-05 16:39:18 -0400

    Fixed a mistake in column no. for Kipp's distance
    Fixed a mistake in .R file 2_morphometric_data_processing.R that had renamed the wrong column (discovered when running .R file 3_LinearModelComparisons_MuMIn.R

--- 2_morphometric_data_processing.R
+++ 2_morphometric_data_processing.R
@@ -7,7 +7,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 ### Read in morphological data set
 morpho <- read.csv("Tyrannus_voucher_table.csv")
-colnames(morpho)[22]<-"Kipp.s.Distance" #rename Kipp.s.Average column for clarity
+colnames(morpho)[23]<-"Kipp.s.Distance" #rename Kipp.s.Average column for clarity
 morpho$Kipp.s.Index<-((morpho$Kipp.s.Distance/morpho$WC.Average)*100) #calculate and add Kipp's Index column
 
 ###NOTE/\/\/\/\/\/\/\/\/\/\/\###
