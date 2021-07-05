7cddc6fcb1b001ddca164dd6d91bfd30ee597552
Maggie MacPherson <maggie.macpherson@gmail.com>
2021-07-05 14:41:36 -0400

    Redid LM comparison using MuMIn package
    Cleaned up .R file to remove old code and run dredge function to get output for top models including either sex and/or age. Saved as Dredge_LMOutput_xxxx.txt files.

--- LinearModels_Demographic_Influence.R
+++ LinearModels_Demographic_Influence.R
@@ -14,73 +14,6 @@ names(table(morpho$Sex))
 morpho<-morpho[!morpho$Sex=="Unknown",] #removes unknown sex individuals
 names(table(morpho$Sex)) #should now only have "Female" and "Male"
 
-### For Use in later steps: Make separate male and female voucher tables
-morpho_Females<-morpho[!morpho$Sex=="Male",]
-write.csv(morpho_Females, file="Voucher_Table_Females.csv")
-
-morpho_Males<-morpho[!morpho$Sex=="Female",]
-write.csv(morpho_Males, file="Voucher_Table_Males.csv")
-
-#response_var<-colnames(morpho)[c(18,19,20,21,22,23,24)]
-response_var<-colnames(morpho)[c(19,20,21,22,23,24,25)]
-
-#Create empty list to store output of each for loop iteration
-LM1_output<-list()
-LM2_output<-list()
-
-for(i in 1:length(response_var)){
-  print(paste0("Character",i," -- ",response_var[i]))#Report character
-  character_of_interest<-morpho[,response_var[i]] #Extract character of interest (coi)
-  names(character_of_interest)<-morpho$tip.label #Add names to vector of coi
-  LM1_output[[i]]<-AIC(lm(morpho[,response_var[i]]~morpho$tip.label, data=morpho)) #Calculate AIC scores
-  LM2_output[[i]]<-AIC(lm(morpho[,response_var[i]]~morpho$tip.label*morpho$Sex, data=morpho)) #Calculate AIC scores while accounting for sex class
-}
-
-names(LM1_output)<-response_var
-names(LM2_output)<-response_var 
-
-AIC_Diff<-list()
-
-for(i in 1:length(response_var)){
-  print(paste0("Character",i," -- ",response_var[[i]]))
-  AIC_Diff[[i]]<-LM1_output[[i]]-LM2_output[[i]]
-}
-
-names(AIC_Diff)<-response_var
-
-### Write out output ###
-sink("Sex_Influence.txt")
-for(i in 1:length(AIC_Diff)){
-  cat(paste0("Character ",i," -- ",response_var[i]))
-  cat("\n\n")
-  print(AIC_Diff[[i]])
-  cat("#############")
-  cat("\n\n")
-}
-sink()
-#Note: For all values >2 AIC units, one model is better than the other.
-#Here, we subtracted LM2 (accounted for sex class) from LM1 (did not account for sex class).
-#All negative values indicate that the model was improved by the accounting of sex class, and positive values that it was not.
-
-###/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\###
-###############################################################
-###/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\###
-
-### Empty global environment to assess the role of Age
-#Note: we do this because some individuals may have unknown sexes (which were previously removed, above) but known ages.
-rm(list=ls())
-
-morpho<-read.csv('./Output Files/Tyrannus morphology data.csv')
-
-### Remove any individuals with unresolved taxonomic identities
-#morpho<-morpho[-grep("unknown",morpho$subspecies),]
-
-### Classify each taxon using trinomial naming
-#morpho$subspecies<-trimws(morpho$subspecies)
-#morpho$trinomial<-paste(morpho$Species,morpho$subspecies)
-#morpho$trinomial<-trimws(morpho$trinomial)
-#names(table(morpho$trinomial)) #see that unknown subspecies were removed
-
 ### Standardize notation for Age column
 names(table(morpho$Age)) #to see which assignments are in the Age column
 morpho$Age[morpho$Age=="SecondYear"]<-"Adult"
@@ -90,42 +23,52 @@ morpho$Age[morpho$Age=="Nestling"]<-"Juvenile"
 morpho<-morpho[!morpho$Age=="Unknown",] #removes unknown age individuals
 names(table(morpho$Age)) #should now only have "Adult" and "Juvenile"
 
-response_var<-colnames(morpho)[c(19,20,21,22,23,24,25)]
+### For Use in later steps: Make separate male and female voucher tables
+morpho_Females<-morpho[!morpho$Sex=="Male",]
+write.csv(morpho_Females, file="Voucher_Table_Females.csv")
+
+morpho_Males<-morpho[!morpho$Sex=="Female",]
+write.csv(morpho_Males, file="Voucher_Table_Males.csv")
 
 #Create empty list to store output of each for loop iteration
-LM1_output<-list()
-LM2_output<-list()
+LM1_output<-list() #Affect of Sex Class
+LM2_output<-list() #Affect of Age Class
+
+options(na.action = "na.fail")
+morpho1<-na.omit(morpho) #we have to remove all the NAs to run the dredge()
+response_var<-colnames(morpho1)[c(19,20,21,22,23,24,25)]
 
 for(i in 1:length(response_var)){
   print(paste0("Character",i," -- ",response_var[i]))#Report character
-  character_of_interest<-morpho[,response_var[i]] #Extract character of interest (coi)
-  names(character_of_interest)<-morpho$tip.label #Add names to vector of coi
-  LM1_output[[i]]<-AIC(lm(morpho[,response_var[i]]~morpho$tip.label, data=morpho)) #Calculate AIC scores
-  LM2_output[[i]]<-AIC(lm(morpho[,response_var[i]]~morpho$tip.label*morpho$Age, data=morpho))
+  character_of_interest<-morpho1[,response_var[i]] #Extract character of interest (coi)
+  names(character_of_interest)<-morpho1$tip.label #Add names to vector of coi 
+  birds.fullmodel_Sex<-lm(morpho1[,response_var[i]]~morpho1$Sex, data = morpho1)
+  birds.fullmodel_Age<-lm(morpho1[,response_var[i]]~morpho1$Age, data = morpho1)
+  LM1_output[[i]]<-dredge(birds.fullmodel_Sex, rank = "AIC")
+  LM2_output[[i]]<-dredge(birds.fullmodel_Age, rank = "AIC")
 }
 
-names(LM1_output)<-response_var
-names(LM2_output)<-response_var 
-
-AIC_Diff<-list()
-
-for(i in 1:length(response_var)){
-  print(paste0("Character",i," -- ",response_var[[i]]))
-  AIC_Diff[[i]]<-LM1_output[[i]]-LM2_output[[i]]
-}
-
-names(AIC_Diff)<-response_var
+names(LM1_output)<-response_var 
+names(LM2_output)<-response_var
 
 ### Write out output ###
-sink("Age_Influence.txt")
-for(i in 1:length(AIC_Diff)){
+sink("Dredge_LMOutput_Sex.txt")
+for(i in 1:length(LM1_output)){
   cat(paste0("Character ",i," -- ",response_var[i]))
   cat("\n\n")
-  print(AIC_Diff[[i]])
+  print(LM1_output[[i]])
   cat("#############")
   cat("\n\n")
 }
 sink()
-#Note: For all values >2 AIC units, one model is better than the other.
-#Here, we subtracted LM2 (accounted for age class) from LM1 (did not account for age class).
-#All negative values indicate that the model was improved by the accounting of age class, and positive values that it was not.
+
+
+sink("Dredge_LMOutput_Age.txt")
+for(i in 1:length(LM2_output)){
+  cat(paste0("Character ",i," -- ",response_var[i]))
+  cat("\n\n")
+  print(LM2_output[[i]])
+  cat("#############")
+  cat("\n\n")
+}
+sink()
