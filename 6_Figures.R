e7073b1856908a43b936c6a00db8689fd44cecc4
Maggie MacPherson <maggie.macpherson@gmail.com>
2021-07-09 13:21:19 -0400

    Remade PPCA figure with WC
    Kipp's distance was not significantly different between migratory and sedentary adult males after bonferroni correction so decided to switch the main figure to show wing chord length instead on the ancestral state reconstruction.

--- 6_Figures.R
+++ 6_Figures.R
@@ -23,9 +23,9 @@ residuals<-read.csv('./Output Files/phylANOVA_tarsus-corrected_residuals.csv', r
 #otu_cv_avg<-read.csv('./Output Files/cv_summary_Females_Adults.csv', row.names = 1) 
 #residuals<-read.csv('./Output Files/phylANOVA_tarsus-corrected_residuals_Females.csv', row.names = 1)
 ### OR
-#otu_avg<-read.csv('./Output Files/Tyrannus morphology + PCA avg_Males.csv', row.names = 1)
-#otu_cv_avg<-read.csv('./Output Files/cv_summary_Males.csv', row.names = 1) 
-#residuals<-read.csv('./Output Files/phylANOVA_tarsus-corrected_residuals_Males.csv', row.names = 1)
+otu_avg<-read.csv('./Output Files/Tyrannus morphology + PCA avg_Males_Adults.csv', row.names = 1)
+otu_cv_avg<-read.csv('./Output Files/cv_summary_Males_Adults.csv', row.names = 1) 
+residuals<-read.csv('./Output Files/phylANOVA_tarsus-corrected_residuals_Males.csv', row.names = 1)
 
 ###/\/\/\/\/\/\/\/\/\/\/\###
 ###########################
@@ -42,7 +42,7 @@ png(file="Phenotype Residuals.png",width=7,height=5.5,units="in",res=500)
 ### Note: If assessing sexes separately, use the following lines of code instead of the above 3 lines, then proceed.
 #png(file="Phenotype Residuals_Females.png",width=7,height=5.5,units="in",res=500)
 ### OR
-#png(file="Phenotype Residuals_Males.png",width=7,height=5.5,units="in",res=500)
+png(file="Phenotype Residuals_Males.png",width=7,height=5.5,units="in",res=500)
 
 ###/\/\/\/\/\/\/\/\/\/\/\###
 ###########################
@@ -110,7 +110,7 @@ png(file="Phenotype Variation.png",width=7,height=5.5,units="in",res=500)
 ### Note: If assessing sexes separately, use the following lines of code instead of the above 3 lines, then proceed.
 #png(file="Phenotype Variation_Females.png",width=7,height=5.5,units="in",res=500)
 ### OR
-#png(file="Phenotype Variation_Males.png",width=7,height=5.5,units="in",res=500)
+png(file="Phenotype Variation_Males.png",width=7,height=5.5,units="in",res=500)
 
 ###/\/\/\/\/\/\/\/\/\/\/\###
 ###########################
@@ -161,7 +161,7 @@ png(file="Phenotype Residuals and Variation.png", width=7, height=5.5, units = "
 ### Note: If assessing sexes separately, use the following lines of code instead of the above 3 lines, then proceed.
 #png(file="Phenotype Residuals and Variation_Females.png", width=7, height=5.5, units = "in", res=500)
 ### OR
-#png(file="Phenotype Residuals and Variation_Males.png", width=7, height=5.5, units = "in", res=500)
+png(file="Phenotype Residuals and Variation_Males.png", width=7, height=5.5, units = "in", res=500)
 
 ###/\/\/\/\/\/\/\/\/\/\/\###
 ###########################
@@ -176,7 +176,7 @@ rl<-lapply(list("./Output Files/Phenotype Residuals.png", "./Output Files/Phenot
 ### Note: If assessing sexes separately, use the following lines of code instead of the above 3 lines, then proceed.
 #rl<-lapply(list("./Output Files/Phenotype Residuals_Females.png", "./Output Files/Phenotype Variation_Females.png"), png::readPNG)
 ### OR
-#rl<-lapply(list("./Output Files/Phenotype Residuals_Males.png", "./Output Files/Phenotype Variation_Males.png"), png::readPNG)
+rl<-lapply(list("./Output Files/Phenotype Residuals_Males.png", "./Output Files/Phenotype Variation_Males.png"), png::readPNG)
 
 ###/\/\/\/\/\/\/\/\/\/\/\###
 ###########################
@@ -197,7 +197,7 @@ png(file="PPC scores boxplots.png",width=7,height=2.75,units="in",res=500)
 ### Note: If assessing sexes separately, use the following lines of code instead of the above 3 lines, then proceed.
 #png(file="PPC scores boxplots_Females.png",width=7,height=2.75,units="in",res=500)
 ### OR
-#png(file="PPC scores boxplots_Males.png",width=7,height=5.5,units="in",res=500)
+png(file="PPC scores boxplots_Males.png",width=7,height=2.75,units="in",res=500)
 
 ###/\/\/\/\/\/\/\/\/\/\/\###
 ###########################
@@ -227,24 +227,6 @@ ggdraw() +
   draw_plot_label(c("A", "B"), c(0.085,0.565), c(0.99,0.99), size=15) + #main plot identifiers 
   draw_plot_label(c("A", "A", "A"), c(0.146, 0.287, 0.420), c(0.87, 0.87, 0.87), size=10) + #letters for plot A
   draw_plot_label(c("A", "AB", "B"), c(0.625, 0.746, 0.9), c(0.87, 0.87, 0.87), size=10) #letters for plot B
-
-
-###NOTE/\/\/\/\/\/\/\/\/\/\/\###
-###########################
-###/\/\/\/\/\/\/\/\/\/\/\###
-
-### Note: If assessing females separately, use the following lines of code instead of the above 3 lines, then proceed.
-#ggdraw() +  
-#  draw_plot(p,0,0,1,1) +
-#  draw_plot_label(c("A", "B", "C", "D"), c(0.085,0.565,0.085,0.565), c(0.99,0.99,0.53,0.53), size=15) + #main plot identifiers 
-#  draw_plot_label(c("A", "A", "A"), c(0.156, 0.287, 0.420), c(0.946, 0.946, 0.946), size=8) + #letters for plot A
-#  draw_plot_label(c("A", "AB", "B"), c(0.635, 0.766, 0.9), c(0.946, 0.946, 0.946), size=8) + #letters for plot B
-#  draw_plot_label(c("A", "A", "A"), c(0.156, 0.287, 0.420), c(0.487, 0.487, 0.487), size=8) + #letters for plot C
-#  draw_plot_label(c("A", "A", "A"), c(0.635, 0.766, 0.9), c(0.487, 0.487, 0.487), size=8) #letters for plot D
-
-###/\/\/\/\/\/\/\/\/\/\/\###
-###########################
-###END/\/\/\/\/\/\/\/\/\/\/\###
 
 dev.off()
 
